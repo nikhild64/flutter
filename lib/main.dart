4dc94ac1653190efe4a987733036aed6cc2e9f57
Your Name <you@example.com>
2020-11-08 15:15:10 +0100

    changed from stateless to stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,44 @@ void main() {
   runApp(Myapp());
 }
 
-class Myapp extends StatelessWidget {
+class Myapp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<Myapp> {
+  var questions = ['Favorite color', 'Favorite animal'];
+
+  var index = 0;
+  void answerQuestion() {
+    print(index);
+
+    setState(() {
+      if (index == questions.length - 1) {
+        index = 0;
+      } else {
+        index = index + 1;
+      }
+    });
+    print(index);
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('firstApp'),
         ),
-        body: Text('Default'),
+        body: Column(
+          children: [
+            Text(questions[index]),
+            RaisedButton(child: Text('Answer1'), onPressed: answerQuestion),
+            RaisedButton(child: Text('Answer1'), onPressed: answerQuestion),
+            RaisedButton(child: Text('Answer1'), onPressed: answerQuestion),
+          ],
+        ),
       ),
     );
   }
